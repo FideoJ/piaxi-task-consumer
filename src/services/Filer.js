6c3b3c17645cfe285e21e391265157ae759e9c88
林剑 <fideojlin@gmail.com>
2018-06-24 02:02:08 +0800

    修复url拼接错误的bug

--- src/services/Filer.js
+++ src/services/Filer.js
@@ -1,10 +1,11 @@
 const request = require('request');
 const { filer } = require('../config');
 const pipeline = require('bluebird').promisify(require('stream').pipeline);
+const urljoin = require('url-join');
 
 class Filer {
   static async download(filePath, stream) {
-    const remote = filer.url + filePath;
+    const remote = urljoin(filer.url, filePath);
     return pipeline(request(remote)
       .on('response', function onResponse(res) {
         if (res.statusCode > 300)
@@ -13,7 +14,7 @@ class Filer {
   }
 
   static async upload(filePath, stream) {
-    const remote = filer.url + filePath;
+    const remote = urljoin(filer.url, filePath);
     return pipeline(stream, request.post(remote));
   }
 }
